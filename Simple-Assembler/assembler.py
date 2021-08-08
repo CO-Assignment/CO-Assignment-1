93b9addc5ad1c2f6baa4c547e9e604c33736984a
Naman Kaushik <kaushiknaman.dk@gmail.com>
2021-08-08 16:31:54 +0100

    opcode mapping and regs

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -1,6 +1,32 @@
 instructions = []
+opcodes = {"add":"00000", "sub":"00001", "ld":"00100", "st":"00101", "mul":"00110", "div":"00111", "rs":"01000", "ls":"01001", "xor":"01010", "or":"01011", "and":"01100", "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"10000", "jgt":"10001", "je":"10010", "hlt":"10011"}
+# mov not added due to conlict
 
-print("helo helo")
+
+r0 = [0]*16
+r1 = [0]*16
+r2 = [0]*16
+r3 = [0]*16
+r4 = [0]*16
+r5 = [0]*16
+r6 = [0]*16
+flags = [0]*4
+
+def convertToDecimal(bin_str):
+    """Handles binary number as strings"""
+    bin_num = str(bin_str)
+    print(bin_num)
+    toRet = 0
+    n = len(bin_num)
+    for i in range(n):
+        if(bin_num[n-i-1]=="1"):
+            toRet += pow(2,i)
+        else:
+            continue
+    return toRet
+
+
+
 
 while True:
     inst = input()
@@ -10,4 +36,4 @@ while True:
     instructions.append(inst)
     if inst=="hlt":
         break
-    # if(len(instructions)>256):
+    
