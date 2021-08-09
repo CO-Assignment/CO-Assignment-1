258ebb06ee068a8ab7594b2499cc18667c7b004d
Heemank Verma <vheemank@gmail.com>
2021-08-09 16:01:31 +0530

    minor changes and error handling

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -1,5 +1,26 @@
 instructions = []
-opcodes = {"add":"00000", "sub":"00001", "ld":"00100", "st":"00101", "mul":"00110", "div":"00111", "rs":"01000", "ls":"01001", "xor":"01010", "or":"01011", "and":"01100", "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"10000", "jgt":"10001", "je":"10010", "hlt":"10011"}
+opcodes = {
+    "add":"00000",
+    "sub":"00001",
+    "movI":"00010",
+    "movR":"00011",
+    "ld":"00100",
+    "st":"00101",
+    "mul":"00110",
+    "div":"00111",
+    "rs":"01000",
+    "ls":"01001",
+    "xor":"01010",
+    "or":"01011",
+    "and":"01100",
+    "not":"01101",
+    "cmp":"01110",
+    "jmp":"01111",
+    "jlt":"10000",
+    "jgt":"10001",
+    "je":"10010",
+    "hlt":"10011"
+    }
 # mov not added due to conlict
 
 r0 = [0]*16
@@ -36,9 +57,7 @@ while True:
     
     inst = input()
     if len(instructions)==256:
-        print("Memory overflow! 256 lines limit has been reached!")
-        #Throw an error here
-        break
+        raise Exception("Memory overflow! 256 lines limit has been reached!")
     
     if(varsDone==False and inst[0:3]!="var"):
         
@@ -48,8 +67,8 @@ while True:
         continue
     
     if(inst[0:3]=="var" and varsDone==True):
-        print("Variables should only be declared in the starting.")
-        break
+        raise Exception("Variables should only be declared in the starting.")
+
     instructions.append(inst)
     if inst=="hlt":
         break
