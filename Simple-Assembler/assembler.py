3eb628f19af326376107400305a1cc3377722647
Naman Kaushik <kaushiknaman.dk@gmail.com>
2021-08-09 10:47:05 +0100

    var input handled

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -2,7 +2,6 @@ instructions = []
 opcodes = {"add":"00000", "sub":"00001", "ld":"00100", "st":"00101", "mul":"00110", "div":"00111", "rs":"01000", "ls":"01001", "xor":"01010", "or":"01011", "and":"01100", "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"10000", "jgt":"10001", "je":"10010", "hlt":"10011"}
 # mov not added due to conlict
 
-
 r0 = [0]*16
 r1 = [0]*16
 r2 = [0]*16
@@ -11,6 +10,11 @@ r4 = [0]*16
 r5 = [0]*16
 r6 = [0]*16
 flags = [0]*4
+memory = []
+for i in range(256):
+    memory.append([0,0])
+
+
 
 def convertToDecimal(bin_str):
     """Handles binary number as strings"""
@@ -26,14 +30,29 @@ def convertToDecimal(bin_str):
     return toRet
 
 
-
+varsDone = False
 
 while True:
+    
     inst = input()
+    if len(instructions)==256:
+        print("Memory overflow! 256 lines limit has been reached!")
+        #Throw an error here
+        break
+    
+    if(varsDone==False and inst[0:3]!="var"):
+        
+        varsDone = True
+
     if(inst==""):
         continue
-
+    
+    if(inst[0:3]=="var" and varsDone==True):
+        print("Variables should only be declared in the starting.")
+        break
     instructions.append(inst)
     if inst=="hlt":
         break
     
+
+       
