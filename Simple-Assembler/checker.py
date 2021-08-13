c943b9d2c3d9f6f4b48faa0b72a56def7fa7e7a4
yashwardhan <yashwardhan20417@iiitd.ac.in>
2021-08-13 23:45:07 +0530

    Type C Type D error functions made

--- Simple-Assembler/checker.py
+++ Simple-Assembler/checker.py
@@ -1,5 +1,5 @@
 from definitions import *
-
+from helpers import *
 
 def checkA(inst):
     if len(inst) != 4:
@@ -20,12 +20,21 @@ def checkB(inst):
 
 
 def checkC(inst):
-    pass
-
-
+    if not(len(inst) == 3):
+        raise Exception("wrong command given for Type C")
+    if inst[1] in Register.keys() and inst[2] in Register.keys():
+        return True
+    if  not (inst[1] in Register.keys()):
+        raise Exception( f'{inst[1]} not a valid register' )
+    if  not (inst[2] in Register.keys()):
+        raise Exception( f'{inst[2]} not a valid register' )
+    return False
 def checkD(inst):
-    pass
-
-
+    if not(len(inst) == 3):
+        raise Exception("wrong command given for Type D")
+    if (inst[1] == 'FLAGS') or not(inst[1] in Register.keys()):
+        raise Exception(f'{inst[1]}  not a valid value for register')
+    if not (inst[2]  in variables.keys()):
+        raise Exception(f'{inst[2]} not a  valid variable')   
 def checkE(inst):
     pass
