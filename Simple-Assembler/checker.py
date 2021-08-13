fb1a4d6adf27d546686efa5ad9851959c9164b39
Lint Action <lint-action@samuelmeuli.com>
2021-08-13 18:15:31 +0000

    Fix code style issues with Black

--- Simple-Assembler/checker.py
+++ Simple-Assembler/checker.py
@@ -1,6 +1,7 @@
 from definitions import *
 from helpers import *
 
+
 def checkA(inst):
     if len(inst) != 4:
         raise Exception("Type A commands require 3 operands")
@@ -20,21 +21,25 @@ def checkB(inst):
 
 
 def checkC(inst):
-    if not(len(inst) == 3):
+    if not (len(inst) == 3):
         raise Exception("wrong command given for Type C")
     if inst[1] in Register.keys() and inst[2] in Register.keys():
         return True
-    if  not (inst[1] in Register.keys()):
-        raise Exception( f'{inst[1]} not a valid register' )
-    if  not (inst[2] in Register.keys()):
-        raise Exception( f'{inst[2]} not a valid register' )
+    if not (inst[1] in Register.keys()):
+        raise Exception(f"{inst[1]} not a valid register")
+    if not (inst[2] in Register.keys()):
+        raise Exception(f"{inst[2]} not a valid register")
     return False
+
+
 def checkD(inst):
-    if not(len(inst) == 3):
+    if not (len(inst) == 3):
         raise Exception("wrong command given for Type D")
-    if (inst[1] == 'FLAGS') or not(inst[1] in Register.keys()):
-        raise Exception(f'{inst[1]}  not a valid value for register')
-    if not (inst[2]  in variables.keys()):
-        raise Exception(f'{inst[2]} not a  valid variable')   
+    if (inst[1] == "FLAGS") or not (inst[1] in Register.keys()):
+        raise Exception(f"{inst[1]}  not a valid value for register")
+    if not (inst[2] in variables.keys()):
+        raise Exception(f"{inst[2]} not a  valid variable")
+
+
 def checkE(inst):
     pass
