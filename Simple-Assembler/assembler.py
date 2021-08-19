f62f0865a7fba6c96536f8c28d512027327868c2
Lint Action <lint-action@samuelmeuli.com>
2021-08-19 07:48:15 +0000

    Fix code style issues with Black

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -1,5 +1,13 @@
-from definitions import (instructions, flags, labels, variables, output,
-                         opcodes, registerStored, variablesStored)
+from definitions import (
+    instructions,
+    flags,
+    labels,
+    variables,
+    output,
+    opcodes,
+    registerStored,
+    variablesStored,
+)
 from typex import TypeA, TypeB, TypeC, TypeD, TypeE
 from sys import stdin
 from checker import checkA, checkB, checkC, checkD, checkE
@@ -13,12 +21,16 @@ for line in stdin:
     line = line.strip()
     if hltReached:
 
-        if line != '':
-            raise Exception(f"Error in line no {tempLineNo}: hlt should be the last instruction")
+        if line != "":
+            raise Exception(
+                f"Error in line no {tempLineNo}: hlt should be the last instruction"
+            )
         break
 
     if len(instructions) == 256:
-        raise Exception(f"Error in line no {tempLineNo}: Memory overflow! 256 lines limit has been reached!")
+        raise Exception(
+            f"Error in line no {tempLineNo}: Memory overflow! 256 lines limit has been reached!"
+        )
 
     if line == "":
         continue
@@ -27,7 +39,9 @@ for line in stdin:
         varsDone = True
 
     elif varsDone is True and line[0:3] == "var":
-        raise Exception(f"Error in line no {tempLineNo}: Variables should only be declared in the starting.")
+        raise Exception(
+            f"Error in line no {tempLineNo}: Variables should only be declared in the starting."
+        )
 
     if "hlt" in str(line):
 
@@ -35,10 +49,14 @@ for line in stdin:
             lineNo = len(instructions)
 
             indexToSplit = line.index(":")
-            if(" " in line[0:indexToSplit]):
-                raise Exception(""f"Error in line no {tempLineNo}cannot have space between label name and \":\" """)
+            if " " in line[0:indexToSplit]:
+                raise Exception(
+                    ""
+                    f'Error in line no {tempLineNo}cannot have space between label name and ":" '
+                    ""
+                )
             labels[line[0:indexToSplit]] = lineNo
-            instructions.append((line[indexToSplit + 1:]).strip())
+            instructions.append((line[indexToSplit + 1 :]).strip())
         else:
             instructions.append(line)
 
@@ -49,10 +67,12 @@ for line in stdin:
         lineNo = len(instructions)
         # TODO: #2 this should be len(instructions) - 1
         indexToSplit = line.index(":")
-        if(" " in line[0:indexToSplit]):
-            raise Exception(f"Error in line no {tempLineNo}: cannot have space between label name and \":\"")
+        if " " in line[0:indexToSplit]:
+            raise Exception(
+                f'Error in line no {tempLineNo}: cannot have space between label name and ":"'
+            )
         labels[line[0:indexToSplit]] = lineNo
-        instructions.append((line[indexToSplit + 1:]).strip())
+        instructions.append((line[indexToSplit + 1 :]).strip())
         continue
 
     # TODO: #7 major error handling left
@@ -76,7 +96,7 @@ for i in range(count):
     k = (instructions[i]).split()
     # TODO: Illegal variable error handling
     # TODO: Viva
-    if (len(k) != 2):
+    if len(k) != 2:
         raise Exception(f"Error in line {i+1} Invalid syntax for variable declaration ")
     variables[k[1]] = numberOfLines + i
     variablesStored[k[1]] = 0
@@ -111,7 +131,7 @@ while j < len(realInstructions):
         or curOp == "or"
         or curOp == "and"
     ) and checkA(i, j):
-        output.append(TypeA(i,j))
+        output.append(TypeA(i, j))
 
     # Type B handling
     # handling mov
@@ -122,7 +142,7 @@ while j < len(realInstructions):
             output.append(TypeC(i, j))
 
     # handling rest of TypeB
-    elif ((curOp == "rs" or curOp == "ls") and checkB(i, j)):
+    elif (curOp == "rs" or curOp == "ls") and checkB(i, j):
         output.append(TypeB(i, j))
 
     # TypeC handling
@@ -130,12 +150,13 @@ while j < len(realInstructions):
         output.append(TypeC(i, j))
 
     # TypeD handling
-    elif (curOp == "ld" or curOp == "st") and checkD(i,j):
+    elif (curOp == "ld" or curOp == "st") and checkD(i, j):
         output.append(TypeD(i, j))
 
     # TypeE handling
-    elif ((curOp == "jmp") or (curOp == "jlt") or (curOp == "jgt")
-            or (curOp == "je")) and checkE(i, j):
+    elif (
+        (curOp == "jmp") or (curOp == "jlt") or (curOp == "jgt") or (curOp == "je")
+    ) and checkE(i, j):
         result = TypeE(i, currFlagState, j)
         if result[0] == -1:
             output.append(result[1])
--- Simple-Assembler/checker.py
+++ Simple-Assembler/checker.py
@@ -3,48 +3,68 @@ from definitions import registerStored, Register, variables, labels
 
 def checkA(inst, j):
     if len(inst) != 4:
-        raise Exception(f"Error in line no {j+1+len(variables)}: Type A commands require 3 operands")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: Type A commands require 3 operands"
+        )
 
     if (
         (inst[1] not in registerStored)
         or (inst[2] not in registerStored)
         or (inst[3] not in registerStored)
     ):
-        raise Exception(f"Error in line no {j+1+len(variables)}: Invalid register provided")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: Invalid register provided"
+        )
     if (inst[1] == "FLAGS") or (inst[2] == "FLAGS") or (inst[3] == "FLAGS"):
-        raise Exception(f"Error in line no {j+1+len(variables)}: Cannot use type A instruction for FLAG register")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: Cannot use type A instruction for FLAG register"
+        )
 
     return True
 
 
 def checkB(inst, j):
     if len(inst) != 3:
-        raise Exception(f"Error in line no {j+1+len(variables)}: wrong command given for Type B")
-    if ((inst[1] in Register.keys()) and
-            ('$' in inst[2]) and
-            (int(inst[2][1:]) < 256)):
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: wrong command given for Type B"
+        )
+    if (inst[1] in Register.keys()) and ("$" in inst[2]) and (int(inst[2][1:]) < 256):
 
-        if(inst[1] != 'FLAGS'):
+        if inst[1] != "FLAGS":
             return True
         else:
-            raise Exception(f"Error in line no {j+1+len(variables)}: Cannot write to the flag register.")
+            raise Exception(
+                f"Error in line no {j+1+len(variables)}: Cannot write to the flag register."
+            )
     if inst[1] not in Register.keys():
-        raise Exception(f"Error in line no {j+1+len(variables)}: {inst[1]} not a valid register")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: {inst[1]} not a valid register"
+        )
     else:
-        raise Exception(f"Error in line no {j+1+len(variables)}: invalid immediate value")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: invalid immediate value"
+        )
 
 
 def checkC(inst, j):
     if not (len(inst) == 3):
-        raise Exception(f"Error in line no {j+1+len(variables)}: wrong command given for Type C")
-    if(inst[1] == "FLAGS"):
-        raise Exception(f"Error in line no {j+1+len(variables)}: You cannot write values into the FLAGS register.")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: wrong command given for Type C"
+        )
+    if inst[1] == "FLAGS":
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: You cannot write values into the FLAGS register."
+        )
     if inst[1] in Register.keys() and inst[2] in Register.keys():
         return True
     if not (inst[1] in Register.keys()):
-        raise Exception(inst[1], f"Error in line no {j+1+len(variables)}: not a valid register")
+        raise Exception(
+            inst[1], f"Error in line no {j+1+len(variables)}: not a valid register"
+        )
     if not (inst[2] in Register.keys()):
-        raise Exception(f"Error in line no {j+1+len(variables)}: {inst[2]} not a valid register")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: {inst[2]} not a valid register"
+        )
     return False
 
 
@@ -56,19 +76,29 @@ def checkD(inst, j):
     ):
         return True
     if not (len(inst) == 3):
-        raise Exception(f"Error in line no {j+1+len(variables)}: wrong command given for Type D")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: wrong command given for Type D"
+        )
     elif (inst[1] == "FLAGS") or not (inst[1] in Register.keys()):
-        raise Exception(f"Error in line no {j+1+len(variables)}: {inst[1]} not a valid value for register")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: {inst[1]} not a valid value for register"
+        )
     elif not (inst[2] in variables.keys()):
-        raise Exception( f"Error in line no {j+1+len(variables)}: {inst[2]} not a  valid variable")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: {inst[2]} not a  valid variable"
+        )
     return False
 
 
 def checkE(inst, j):
     if len(inst) != 2:
-        raise Exception(f"Error in line no {j+1+len(variables)}: Type E commands have only 1 operand which is the memory address ( {len(inst)-1} ) specified")
+        raise Exception(
+            f"Error in line no {j+1+len(variables)}: Type E commands have only 1 operand which is the memory address ( {len(inst)-1} ) specified"
+        )
     if inst[1] not in labels.keys():
         if inst[1] in variables.keys():
-            raise Exception(f"Error in line no {j+1+len(variables)}: Variable specified instead of label")
+            raise Exception(
+                f"Error in line no {j+1+len(variables)}: Variable specified instead of label"
+            )
         raise Exception(f"Error in line no {j+1+len(variables)}: Label does not exist")
     return True
