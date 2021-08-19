f92aff929b347842bf6abd44c4c56d4585433105
yashwardhan <yashwardhan20417@iiitd.ac.in>
2021-08-19 13:08:58 +0530

    Type C done

--- SimpleSimulator/s_typeX.py
+++ SimpleSimulator/s_typeX.py
@@ -50,9 +50,45 @@ def sTypeA(i):
 def sTypeB(i):
     pass
 
-def sTypeC(i):
-    pass
+def sTypeC(i, currFlag):
+    opcode = i[0:5]
+    reg1 = i[10:13]
+    reg2 = i[13:]
+    if (opcodes[opcode] == "cmp"):
+        val1 = registerStored[reg1]
+        val2 = registerStored[reg2]
+        if (val1 > val2):
+            registerStored["111"] = 2
+        elif (val1 < val2):
+            registerStored["111"] = 4
+        else:
+            registerStored["111"] = 1
 
+    elif (opcodes[opcode] == "not"):
+        noToFlip = convertToBin(registerStored[reg2], 16)
+        
+        inverting = ""
+        for ch in range(len(noToFlip)):
+            if(noToFlip[ch] == "1"):
+                inverting = inverting + "0"
+            else:
+                inverting = inverting + "1"
+        
+        flippedNo = convertToDecimal(inverting)
+        
+        registerStored[reg1] = flippedNo
+
+    elif (opcodes[opcode] == "div"):
+        quotient = (registerStored[reg1]) // (registerStored[reg2])
+        remainder = registerStored[reg1] % registerStored[reg2]
+        registerStored["000"] = quotient
+        registerStored["001"] = remainder
+
+    elif (opcodes[opcode] == "movR"):
+        if(reg2 == "111"):
+            registerStored[reg1] = currFlag
+            return
+        registerStored[reg1] = registerStored[reg2]
 def sTypeD(i):
     opcode = i[0:5]
     reg = i[5:8]
